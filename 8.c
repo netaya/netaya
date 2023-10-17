f9dd1869ff02161271c81f060bf355647c279ce9
netaya <146194084+netaya@users.noreply.github.com>
2023-10-17 22:06:59 +0800

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	if (a == 1) { printf("yes\n"); }
+	else if (a == 2) { printf("no\n"); }
+	else { printf("error\n"); }
+
+
+	return 0;
+}
